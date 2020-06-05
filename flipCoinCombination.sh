7d14f1e0579f6178258b55e7d82dfd69d2724582
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 16:54:23 +0530

    Percentage of Heads & Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,11 +2,28 @@
 
 echo Welcome to Flip coin Combination
 
-coin=$((RANDOM%2))
-if [ $coin -eq 0 ]
-then
-	echo heads
-else
-	echo tails
-fi
-
+heads=0
+tails=0
+declare -A singlet
+for ((i=1; i<=10; i++))
+do
+        coin=$((RANDOM%2))
+        if [ $coin -eq 0 ]
+        then
+                singlet[c"$i"]="head"
+                ((heads++))
+        else
+                singlet[c"$i"]="tail"
+                ((tails++))
+        fi
+done
+for key in ${!singlet[@]}
+do
+        echo "$key : ${singlet[$key]}"
+done
+h=$(echo "scale=2; $heads/10" | bc )
+t=$(echo "scale=2; $tails/10" | bc )
+heads_percent=$(echo "scale=2; $h*100" | bc )
+tails_percent=$(echo "scale=2; $t*100" | bc )
+echo $heads_percent % of Heads
+echo $tails_percent % of Tails
