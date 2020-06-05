854dcd4169f21de8c081ac15e88d9cc7b4d21f70
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-05 17:06:09 +0530

    Percentage of Singlet and Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,28 +2,64 @@
 
 echo Welcome to Flip coin Combination
 
-heads=0
-tails=0
-declare -A singlet
-for ((i=1; i<=10; i++))
+H=0
+T=0
+HH=0
+TT=0
+HT=0
+TH=0
+read -p "How many times do you want to flip the coins?   " flip
+declare -A combination
+for ((i=1; i<=$flip; i++))
 do
         coin=$((RANDOM%2))
         if [ $coin -eq 0 ]
         then
-                singlet[c"$i"]="head"
-                ((heads++))
+               ((H++))
         else
-                singlet[c"$i"]="tail"
-                ((tails++))
+               ((T++))
         fi
 done
-for key in ${!singlet[@]}
+
+for ((i=1; i<=$flip; i++))
 do
-        echo "$key : ${singlet[$key]}"
+        coin1=$((RANDOM%2))
+        coin2=$((RANDOM%2))
+        if [ $coin1 -eq 0 -a $coin2 -eq 0 ]
+        then
+               ((HH++))
+        elif [ $coin1 -eq 1 -a $coin2 -eq 1 ]
+        then
+               ((TT++))
+        elif [ $coin1 -eq 0 -a $coin2 -eq 1 ]
+        then
+                ((HT++))
+        elif [ $coin1 -eq 1 -a $coin2 -eq 0 ]
+        then
+                ((TH++))
+        fi
 done
-h=$(echo "scale=2; $heads/10" | bc )
-t=$(echo "scale=2; $tails/10" | bc )
-heads_percent=$(echo "scale=2; $h*100" | bc )
-tails_percent=$(echo "scale=2; $t*100" | bc )
-echo $heads_percent % of Heads
-echo $tails_percent % of Tails
+h=$(echo "scale=2; $H/$flip" | bc )
+t=$(echo "scale=2; $T/$flip" | bc )
+hh=$(echo "scale=2; $HH/$flip" | bc )
+tt=$(echo "scale=2; $TT/$flip" | bc )
+ht=$(echo "scale=2; $HT/$flip" | bc )
+th=$(echo "scale=2; $TH/$flip" | bc )
+
+H_percent=$(echo "scale=2; $h*100" | bc )
+T_percent=$(echo "scale=2; $t*100" | bc )
+HH_percent=$(echo "scale=2; $hh*100" | bc )
+TT_percent=$(echo "scale=2; $tt*100" | bc )
+HT_percent=$(echo "scale=2; $ht*100" | bc )
+TH_percent=$(echo "scale=2; $th*100" | bc )
+
+combination["H"]=$H_percent
+combination["T"]=$T_percent
+combination["HH"]=$HH_percent
+combination["TT"]=$TT_percent
+combination["HT"]=$HT_percent
+combination["TH"]=$TH_percent
+for key in ${!combination[@]}
+do
+        echo "$key : ${combination[$key]} %"
+done
